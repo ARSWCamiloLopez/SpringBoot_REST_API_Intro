f0e4c26380f396aef70a1acde4aade53fc59f8b5
Camilo Lopez <camilino-123@hotmail.com>
2018-10-15 09:29:07 -0500

    Punto 3 parte 3 terminado

--- src/test/java/edu/eci/arsw/myrestaurant/test/ApplicationServicesTests.java
+++ src/test/java/edu/eci/arsw/myrestaurant/test/ApplicationServicesTests.java
@@ -3,9 +3,11 @@ package edu.eci.arsw.myrestaurant.test;
 import edu.eci.arsw.myrestaurant.beans.BillCalculator;
 import edu.eci.arsw.myrestaurant.model.Order;
 import edu.eci.arsw.myrestaurant.services.OrderServicesException;
+import edu.eci.arsw.myrestaurant.services.RestaurantOrderServices;
 import edu.eci.arsw.myrestaurant.services.RestaurantOrderServicesStub;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,16 +19,42 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest()
 public class ApplicationServicesTests {
 
-    
-    RestaurantOrderServicesStub ros;
+    @Autowired
+    RestaurantOrderServices ros;
 
-    
     @Test
-    public void contextLoads() throws OrderServicesException{
-        
-        
-        
-        
+    public void contextLoads() throws OrderServicesException {
+
+    }
+
+    /**
+     * Verifica que el valor retornado por la funcion calculateTableBill con la
+     * inyeccion de TaxesCalculator2016TributaryReform calcule correctamente el
+     * valor
+     *
+     * @throws OrderServicesException
+     */
+    @Test
+    public void calculateBillTable1WithTaxes() throws OrderServicesException {
+
+        Integer total = ros.calculateTableBill(1);
+
+        assertEquals(total, Integer.valueOf(45302));
+    }
+
+    /**
+     * Verifica que el valor retornado por la funcion calculateTableBill con la
+     * inyeccion de TaxesCalculator2016TributaryReform calcule correctamente el
+     * valor
+     *
+     * @throws OrderServicesException
+     */
+    @Test
+    public void calculateBillTable2WithTaxes() throws OrderServicesException {
+
+        Integer total = ros.calculateTableBill(3);
+
+        assertEquals(total, Integer.valueOf(32290));
     }
 
 }
